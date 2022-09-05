4759bb4339781d5b860271b140073b1aa577e58b
hamza2262 <ha5600330xd@gmail.com>
2022-09-05 16:31:45 +0500

    Emails Sending From Sendgrid

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,10 +9,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  POST
+  def create
+    super
+    # Create the user from params
+    @user = User.new(user_params)
+    if @user.save
+      # Deliver the signup email
+      UserNotifierMailer.send_signup_email(@user).deliver
+      redirect_to(@user, :notice => 'User created')
+    else
+      render :action => 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :login)
+  end
+end
+
 
   # GET /resource/edit
   # def edit
