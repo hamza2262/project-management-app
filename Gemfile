45017fc87acae85500a85492a4a40cf83197f848
hamza2262 <ha5600330xd@gmail.com>
2022-09-17 14:51:09 +0500

    Some changes in carrierwave.rb file

--- Gemfile
+++ Gemfile
@@ -29,7 +29,6 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem "mini_magick"
 gem 'carrierwave'
-gem 'fog', group: :production
 gem 'fog-aws', group: :production
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-
-  if Rails.env.staging? || Rails.env.production?
-    CarrierWave.configure do |config|
-     config.fog_provider = ‘fog/aws’
-     config.fog_credentials = {
-       provider: 'AWS',
-       aws_access_key_id: ENV['S3_KEY'],
-       aws_secret_access_key: ENV['S3_SECRET']
-       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-      }
-     config.fog_directory = ENV['S3_BUCKET']
-     config.storage = :fog
-     config.storage = :file
-    end
+if Rails.env.staging? || Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET']
+      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+    # config.storage = :file
   end
+end
