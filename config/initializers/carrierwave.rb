8fe55cef19cc05c792751ae8e28cb34a0abcdbac
hamza2262 <ha5600330xd@gmail.com>
2022-09-17 15:02:23 +0500

    Some changes in carrierwave.rb file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ if Rails.env.staging? || Rails.env.production?
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET']
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+      aws_secret_access_key: ENV['S3_SECRET'],
+      region: ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
     }
     config.fog_directory = ENV['S3_BUCKET']
     # config.storage = :file
