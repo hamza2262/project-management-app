e631c697a7dd3b994ad369ddc4f65876c2bec04c
hamza2262 <ha5600330xd@gmail.com>
2022-10-01 01:09:10 +0500

    Delete member_email and member_name from members migration file

--- db/migrate/20220922060107_create_members.rb
+++ db/migrate/20220922060107_create_members.rb
@@ -1,8 +1,7 @@
 class CreateMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :members do |t|
-      t.string :member_name
-      t.string :member_email
+
       t.timestamps
     end
   end
