4bd170cd1eb49289469e82e97481c93c6d277381
hamza2262 <ha5600330xd@gmail.com>
2022-10-01 11:30:09 +0500

    If_not_exists add

--- db/migrate/20220922060210_adding_member_attributes.rb
+++ db/migrate/20220922060210_adding_member_attributes.rb
@@ -1,7 +1,7 @@
 class AddingMemberAttributes < ActiveRecord::Migration[6.1]
   def change
-    add_column :members, :member_id, :integer
-    add_column :members, :member_name, :string, null: false
-    add_column :members, :member_email, :string, null: false
+    add_column :members, :member_id, :integer, if_not_exists: true
+    add_column :members, :member_name, :string, null: false , if_not_exists: true
+    add_column :members, :member_email, :string, null: false , if_not_exists: true
   end
 end
